04d915853656f2a37def95f40570fe5248a07a76
Anja Zrnec <anjazrnec@gmail.com>
2015-01-05 17:33:09 +0100

    V5
    final version

--- Java/Bioinformatics/src/Main.java
+++ Java/Bioinformatics/src/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		//at the beginning of every string there has to be a minus sign for program to work correctly!!!
-		String row = "-ACACACTA";
-		String colomn = "-AGCACACA";
+	//	String row = "-ACACACTA";
+	//	String colomn = "-AGCACACA";
+		String row= args[0];
+		String colomn =args[1];
 		int[][] doneMatrix = new int[row.length()][colomn.length()];
 		MakeMatrix makeMatrix = new MakeMatrix(colomn, row);
 		List<Integer> bestPath = new ArrayList<>();
