c52c43e3d3aba443cca373da3773c721e53cba91
Anja Zrnec <anjazrnec@gmail.com>
2015-01-12 17:38:03 +0100

    Examples

--- Java/Bioinformatics/src/Main.java
+++ Java/Bioinformatics/src/Main.java
@@ -1,15 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		//at the beginning of every string there has to be a minus sign for program to work correctly!!!
+		//input arguments are two files, each file consists of one string!
 	//	String colomn = "-AGTCCGAGGGCTACTCTACTGAAC";
 	//	String row = "-CCAATCTACTACTGCTTGCAGTAC";
-		String row= args[0];
-		String colomn =args[1];
+		String fileName = args[0];
+		String fileName2 = args[1];
+        BufferedReader input = new BufferedReader(new FileReader(fileName));
+        BufferedReader input2 = new BufferedReader(new FileReader(fileName2));
+        String begin = "-";
+        String line = null;
+        String line1 = null;
+        String colomn = null;
+        String row = null;
+        try {
+            while (( line = input.readLine()) != null){
+            	colomn = begin + line;
+                System.out.println(colomn);
+            }
+            while (( line1 = input2.readLine()) != null){
+            	row = begin + line1;
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+
 		int[][] doneMatrix = new int[row.length()][colomn.length()];
 		MakeMatrix makeMatrix = new MakeMatrix(colomn, row);
 		List<Integer> bestPath = new ArrayList<>();
